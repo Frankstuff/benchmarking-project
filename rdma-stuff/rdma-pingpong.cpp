7775e0eb7db80daea15bcfac20c2c21961e3ad4c
frank <fpajak@terpmail.umd.edu>
2026-09-06 19:26:02 -0400

    queried gid and other stuff minor fixes

--- rdma-stuff/rdma-pingpong.cpp
+++ rdma-stuff/rdma-pingpong.cpp
@@ -1,3 +1,33 @@
+/* this is what is left to be done . . .  
+ 1. Query this endpoint's local GID.
+
+ 2. Construct local connection information:
+    - queue_pair->qp_num
+    - local PSN
+    - local GID
+
+ 3. Send local information through control_socket.
+
+ 4. Receive remote information through control_socket.
+
+ 5. Move local QP from INIT to RTR using remote information.
+
+ 6. Move local QP from RTR to RTS using local PSN.
+
+ 7. Post receive work requests.
+
+ 8. Synchronize both processes.
+
+ 9. Start RDMA SEND/RECV.
+
+
+
+*/
+
+
+
+
+
 #include <infiniband/verbs.h> //in order to use ibv_get_device_list, ibv_free_device_list
 #include <cstdio>
 
@@ -12,9 +42,90 @@
 #include <cstring>
 
 //chat gave me some functions to exchange info
-
 constexpr uint16_t TCP_CONTROL_PORT = 18515;
+constexpr uint8_t RDMA_PORT_NUMBER = 1;
+constexpr int GID_INDEX = 1;
 
+
+struct ConnectionData {
+    uint32_t qp_number;
+    uint32_t packet_sequence_number;
+    ibv_gid gid;
+};
+
+//the functions below are just there to make sure that the data is sent even if send and receive fail (im pretty sure this is like 99% unnecessary because the messages i will be exchanging are extremely short
+
+bool send_all(int socket_fd, const void *data, std::size_t length)
+{
+    const char *current =
+        static_cast<const char *>(data);
+
+    std::size_t bytes_sent = 0;
+
+    while (bytes_sent < length) {
+        ssize_t result = send(
+            socket_fd,
+            current + bytes_sent,
+            length - bytes_sent,
+            MSG_NOSIGNAL
+        );
+
+        if (result < 0) {
+            if (errno == EINTR) {
+                continue;
+            }
+
+            std::perror("send");
+            return false;
+        }
+
+        if (result == 0) {
+            return false;
+        }
+
+        bytes_sent += static_cast<std::size_t>(result);
+    }
+
+    return true;
+}
+
+bool receive_all(int socket_fd, void *data, std::size_t length)
+{
+    char *current = static_cast<char *>(data);
+
+    std::size_t bytes_received = 0;
+
+    while (bytes_received < length) {
+        ssize_t result = recv(
+            socket_fd,
+            current + bytes_received,
+            length - bytes_received,
+            0
+        );
+
+        if (result < 0) {
+            if (errno == EINTR) {
+                continue;
+            }
+
+            std::perror("recv");
+            return false;
+        }
+
+        if (result == 0) {
+            std::fprintf(
+                stderr,
+                "Peer closed the TCP connection\n"
+            );
+
+            return false;
+        }
+
+        bytes_received += static_cast<std::size_t>(result);
+    }
+
+    return true;
+}
 int create_server_connection()
 {
     int listening_socket = socket(AF_INET, SOCK_STREAM, 0);
@@ -148,14 +259,14 @@ int create_client_connection(const char *server_ip)
 }
 
 
-int main(argc, char *argv[]) {
+int main(int argc, char *argv[]) {
     int buffer_size = 1024;//I should make this a macro but whatever
     //I will be using a particular set of settings
     struct ibv_device **rdma_devices = ibv_get_device_list(NULL);//returns a null terminated array of devices 
     //there are a couple error codes that I need to handle, but I just won't for now like EPERM  Permission denied.ENOSYS No kernel support for RDMA.ENOMEM Insufficient memory to complete the operation 
     struct ibv_context *pointer_to_device_context = ibv_open_device(*rdma_devices);//device comes from the list of devices . .  I am just using the first one because I don't expect other 
     //devices for now just the rxe one that i created in software, just copied this from the man pages
-
+    ibv_free_device_list(rdma_devices);
     //struct to be filled in that holds the device attributes is below
     struct ibv_device_attr device_attr; //declare
     
@@ -164,7 +275,30 @@ int main(argc, char *argv[]) {
     std::printf("the port number is: %d\n", device_attr.phys_port_cnt);
     struct ibv_port_attr port_attr;   //this is getting filled in
  
-    int query_port = ibv_query_port(pointer_to_device_context, 1, &port_attr);//we are assuming here that the RDMA port number that we want on our device  
+    int query_port = ibv_query_port(pointer_to_device_context, RDMA_PORT_NUMBER, &port_attr);//we are assuming here that the RDMA port number that we want on our device  
+
+    ibv_gid local_gid{};
+    int query_gid_result = ibv_query_gid(pointer_to_device_context, RDMA_PORT_NUMBER, GID_INDEX, &local_gid);
+    if (query_gid_result != 0) {
+        std::perror("ibv_query_gid");
+        return 1;
+    }
+    //we now have the LOCAL RDMA ADDRESS FOR THE RESPECTIVE PROCESS
+
+    char local_gid_string[INET6_ADDRSTRLEN]{};
+
+    const char *conversion_result = inet_ntop(AF_INET6, &local_gid, local_gid_string, sizeof(local_gid_string));
+
+    if (conversion_result == nullptr) {
+        std::perror("inet_ntop for GID");
+        return 1;
+    }
+
+    std::printf("Local GID at index %d: %s\n", GID_INDEX, local_gid_string);
+
+
+
+
 
     //we are gonna create a protected domain below . . . still need to fully understand protection domain. . . 
 
@@ -196,7 +330,10 @@ int main(argc, char *argv[]) {
     int flags = IBV_QP_STATE | IBV_QP_PKEY_INDEX | IBV_QP_PORT | IBV_QP_ACCESS_FLAGS;
     int result = ibv_modify_qp(queue_pair, &attr, flags);
     //the stuff above is apparently usually done as a entire function that moves the qp into an initialized state. . .
-   
+    if (result != 0) {
+        std::fprintf(stderr, "could not change the qp state to init: %s\n", std::strerror(result));
+        return 1;
+    } 
     int control_socket = -1;
 
     if (argc == 1) {
@@ -221,6 +358,11 @@ int main(argc, char *argv[]) {
         std::fprintf(stderr, "Could not establish TCP control connection\n");
 	return 1;
     }
+
+    //now the client and the server have established a tcp connection, but they need to exchange GIDs 
+    //Global identifiers are used to recognize network ports on rdma adapters
+
+    
  
     close(control_socket);
     
